78f398550d5dc33614457eefd0b4cec80f23c48c
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-05-23 14:12:03 +0600

    vercel

--- dist/middleware/globalError.js
+++ dist/middleware/globalError.js
@@ -3,7 +3,7 @@ export const globalError = (err, req, res, next) => {
     res.status(500).json({
         success: false,
         message: err instanceof Error ? err.message : "Internal Server Doesn't Work",
-        stack: config.node_env === "developement" && err instanceof Error ? err.stack : undefined
+        stack: config.node_env === "development" && err instanceof Error ? err.stack : undefined
     });
     next();
 };
